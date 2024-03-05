4bfe85671d2828a8c9acf7aef575b823f469a8d3
KABYA BASU <kabyabasu@yahoo.co.uk>
2024-03-05 19:27:09 +0000

    adding main and req file with values

---
+++ calculation/cal.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+class basicInfo:
+    def __init__(self):
+        pass
+
+    def intro():
+        name = st.text_input("What is you Name")
+        return name
---
+++ main.py
@@ -0,0 +1,5 @@
+from calculation.cal import basicInfo
+
+if __name__ == "__main__":
+    app = basicInfo
+    name = app.intro()
