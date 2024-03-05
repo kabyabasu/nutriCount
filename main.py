db067e12d34ea9306d0ea84fe3aa4d15ca9f1fea
KABYA BASU <kabyabasu@yahoo.co.uk>
2024-03-05 21:30:57 +0000

    resizing imgae

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from src.input.input import basicInfo
 import streamlit as st
 if __name__ == "__main__":
-    st.image("./image/eae413ab-fe80-4cad-862c-584aeed0b894.webp")
+    st.image("./image/eae413ab-fe80-4cad-862c-584aeed0b894.webp",width=10)
     app = basicInfo
     name = app.intro()
     print(name)
