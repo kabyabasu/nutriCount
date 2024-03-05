2a18923214c3fbfa5d5f1c7fcf2907dc09988e9d
KABYA BASU <kabyabasu@yahoo.co.uk>
2024-03-05 21:28:04 +0000

    updated logo

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from src.input.input import basicInfo
 import streamlit as st
 if __name__ == "__main__":
-    st.image("/workspaces/nutriCount/image/eae413ab-fe80-4cad-862c-584aeed0b894.webp")
+    st.image("./image/eae413ab-fe80-4cad-862c-584aeed0b894.webp")
     app = basicInfo
     name = app.intro()
     print(name)
