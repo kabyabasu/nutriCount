63e9fa0fbd3da3fc3a529e2f933eb30ea61ab4f7
KABYA BASU <kabyabasu@yahoo.co.uk>
2024-03-07 00:45:24 +0000

    adding nested structure

---
+++ Profile.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+
+def app():
+
+    Test = st.selectbox(
+        "Are you Ready to Incorporate Exercise in your Daily routine", ["Yes", "No"]
+    )
+ 
--- app.py
+++ app.py
@@ -1,11 +1,42 @@
 import streamlit as st
-import basic
-import lifestyleInformation
+from pathlib import Path
 
-PAGES = {"Basic Information": basic, "Life Style Information": lifestyleInformation}
+# import basic
+# import lifestyleInformation
 
-st.sidebar.title("Navigation")
-selection = st.sidebar.radio("Go to", list(PAGES.keys()))
-page = PAGES[selection]
+# PAGES = {"Basic Information": basic, 
+#          "Life Style Information": lifestyleInformation}
 
-page.app()
+# st.sidebar.title("Navigation")
+# selection = st.sidebar.radio("Go to", list(PAGES.keys()))
+# selection
+# page = PAGES[selection]
+
+# page.app()
+
+
+
+"Contents of `pages_sections.toml`"
+
+st.code(Path("pages_section.toml").read_text(), language="toml")
+
+"Streamlit script:"
+
+with st.echo("below"):
+    from st_pages import show_pages_from_config
+
+    show_pages_from_config("pages_section.toml")
+
+
+with st.expander("Show documentation"):
+    from st_pages import add_indentation
+
+    st.help(show_pages)
+
+    st.help(Page)
+
+    st.help(add_page_title)
+
+    st.help(Section)
+
+    st.help(add_indentation)
