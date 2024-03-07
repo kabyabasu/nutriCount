456e4fde1d08a1e8e95f5db47fc7e6b84bd0ab68
KABYA BASU <kabyabasu@yahoo.co.uk>
2024-03-07 22:44:23 +0000

    data uploaded

--- pages/basic.py
+++ pages/basic.py
@@ -1,10 +1,11 @@
 import streamlit as st
 from st_pages import add_page_title
-from algo.shs import call_back
+#from algo.shs import call_back
 
 add_page_title()
 
-
+def call_back():
+    new_weight = st.session_state.weight
 
 
 
