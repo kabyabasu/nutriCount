d1645d382d0d2b44f5145d49a5515f4661671f1c
KABYA BASU <kabyabasu@yahoo.co.uk>
2024-03-07 07:51:27 +0000

    updating daeb page

--- daeb.py
+++ daeb.py
@@ -17,7 +17,26 @@ def app():
             "Gluten Free",
         ],
     )
-    cnom = st.number_input("How many meals you eat per day",min_value=1,max_value=4)
-    cnom_detail = st.text_input("What are these meals",max_chars=130,help="breakfast")
+    cnom = st.number_input("How many meals you eat per day", min_value=1, max_value=6)
+    cnom_detail = st.text_input("What are these meals", max_chars=130, help="breakfast")
+    first_major_meal = st.selectbox(
+        "What is your first major meal of the Day",
+        [
+            "Breakfast",
+            "Morning Snack",
+            "Brunch",
+            "Lunch",
+            "Afternoon Snack",
+            "Evening Snack",
+            "Dinner",
+        ],
+    )
+    ci = st.number_input(
+        "How Many Cups of Coffee you drink per day", min_value=0, max_value=10
+    )
+    water_intake = st.select_slider(
+        "How many Liters of Water you drink in a day", 0.5, 5.5
+    )
+
 
 app()
