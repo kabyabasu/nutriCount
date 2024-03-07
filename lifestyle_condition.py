a052612feec1b2fb27825ff003bae4de20e2111e
KABYA BASU <kabyabasu@yahoo.co.uk>
2024-03-07 09:55:57 +0000

    updating  ba  page

---
+++ lifestyle_condition.py
@@ -0,0 +1,26 @@
+import streamlit as st
+from st_pages import add_page_title
+
+add_page_title()
+
+
+def app():
+    mc = st.multiselect(
+        "Do you have any of these medical Condition",
+        [
+            "None",
+            "HyperTension",
+            "High Pressure",
+            "Low Pressure",
+            "Heart Disease",
+            "Diabetes",
+            "Obesity",
+            "Malnourishment",
+            "Migraines",
+            "Thyroid",
+            "PCOS",
+            "Insomnia",
+        ],
+        default="None",
+    )
+    mc_add = st.text_input("If you have any medical condtion that is not mentioned above please give details",max_chars=130,disabled=True)
