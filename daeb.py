65fc3bfa59f4c52c2c895542a15d4aff66b7ddab
KABYA BASU <kabyabasu@yahoo.co.uk>
2024-03-07 07:27:14 +0000

    adding daeb page

---
+++ daeb.py
@@ -0,0 +1,22 @@
+import streamlit as st
+from st_pages import add_page_title
+
+add_page_title()
+
+
+def app():
+    diet_style = st.selectbox(
+        "What is your Diet Style",
+        [
+            "Vegan",
+            "Eggetarian",
+            "Vegetarian",
+            "Piscetarian",
+            "Non Vegetarian",
+            "Keto",
+            "Gluten Free",
+        ],
+    )
+    cnom = st.number_input("How many meals you eat per day"min_value=1,max_value=4)
+
+app()
