39efa65f1f0a5a9aa596528908e92e5d77de7816
KABYA BASU <kabyabasu@yahoo.co.uk>
2024-03-07 08:05:54 +0000

    updating daeb page

--- daeb.py
+++ daeb.py
@@ -37,7 +37,7 @@ def app():
     water_intake = st.number_input(
         "How many Liters of Water you drink in a day", 1, 6
     )
-    daily_calories = st.slider("What is your daily Cal Consumption (best guess)",500,5000)
+    daily_calories = st.slider("What is your daily Cal Consumption (best guess)",500,5000,value=3500)
     dails_carbs = st.slider("What % of your total calories from Carb consumption (best guess)",0,100)
     dails_fat = st.slider("What % of your total calories from Fat consumption (best guess)",0,100)
     dails_protein = st.slider("What % of your total calories from Protein consumption (best guess)",0,100)
