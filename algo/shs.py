68ecf25db076bf4b6ee30361424be1408136d6f2
KABYA BASU <kabyabasu@yahoo.co.uk>
2024-03-07 15:51:18 +0000

    adding data

--- algo/shs.py
+++ algo/shs.py
@@ -1,4 +1,5 @@
 import streamlit as st
-from pages.basic import current_weights
+from pages.basic import current_weights,c_weigh
 
 new_weight = current_weights *2
+new_weight_2 = c_weigh
--- pages/Profile.py
+++ pages/Profile.py
@@ -1,6 +1,6 @@
 import streamlit as st
 from st_pages import add_page_title
-from algo.shs import new_weight
+from algo.shs import new_weight,new_weight_2
 add_page_title()
 
 def app():
@@ -8,6 +8,7 @@ def app():
     st.write(
 
         new_weight
+        new_weight_2
     )
 
  
