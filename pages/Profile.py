c75f702bb212949537d2b0912a6372f13c09f631
KABYA BASU <kabyabasu@yahoo.co.uk>
2024-03-07 15:53:15 +0000

    adding data

--- pages/Profile.py
+++ pages/Profile.py
@@ -1,6 +1,7 @@
 import streamlit as st
 from st_pages import add_page_title
-from algo.shs import new_weight,new_weight_2
+from algo.shs import new_weight
+from algo.shs import new_weight_2
 add_page_title()
 
 def app():
