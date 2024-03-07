947a3e7e5e7ebb16aa950dbd4bad43991feda222
KABYA BASU <kabyabasu@yahoo.co.uk>
2024-03-07 09:12:07 +0000

    updating  ba  page

--- ba.py
+++ ba.py
@@ -45,6 +45,57 @@ def app():
         ],
         default="No Pain",
     )
+    tingling = st.selectbox(
+        "Do you have Tingling or Numbness in the body",
+        [
+            "No",
+            "Yes",
+        ],
+    )
+    tingling_intensity = (
+        st.selectbox(
+            "What is the intensity of Tingling or Numbness in the body",
+            [
+                "No Tingling or Numbness",
+                "Mild (It doesn't bother me much but I feel it)",
+                "Moderate  (Feel it everday but I manage)",
+                "Severe (Daily activities are affected)",
+                "Life has no meaning (Need immediate relief)",
+            ],
+        ),
+    )
+
+    tingling_location = st.multiselect(
+        "Where do you have Tingling or Numbness in the body",
+        [
+            "No Tingling or Numbness",
+            "Neck",
+            "Shoulder",
+            "Arm",
+            "Forearm",
+            "Elbow",
+            "Wrist + Hand",
+            "Upper Back",
+            "Lower Back",
+            "Hip & Thigh",
+            "Knee",
+            "Shin",
+            "Ankle",
+            "Foot",
+            "Chest",
+            "Groin",
+            "Stomach",
+        ],
+        default="No Tingling or Numbness",
+    )
+    return (
+        pain,
+        pain_intensity,
+        pain_location,
+        tingling,
+        tingling_intensity,
+        tingling_location,
+    )
 
 
 app()
