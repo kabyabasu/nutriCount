7bc3563aa705d0a887f4b5bfa7bf6daa3dc2b1eb
KABYA BASU <kabyabasu@yahoo.co.uk>
2024-03-07 22:34:29 +0000

    add values

--- algo/shs.py
+++ algo/shs.py
@@ -2,5 +2,4 @@ import streamlit as st
 from pages.basic import form_1
 
 def call_back():
-    new_weight = st.session_state["weight"] *2
-    return new_weight
+    new_weight = st.session_state.weight
