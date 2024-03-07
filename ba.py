a59d2bb0271c3a912b2fe53f5a3a82985bf6c418
KABYA BASU <kabyabasu@yahoo.co.uk>
2024-03-07 08:40:10 +0000

    adding ba  page

---
+++ ba.py
@@ -0,0 +1,14 @@
+import streamlit as st
+from st_pages import add_page_title
+
+add_page_title()
+
+
+def app():
+    pain = st.selectbox(
+        "Do you have Pain in the body",
+        [
+            "Yes",
+            "No",
+        ],
+    )
