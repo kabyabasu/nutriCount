9d7d815131dc037d24101a064d4dd0c48e2856cd
KABYA BASU <kabyabasu@yahoo.co.uk>
2024-03-09 12:25:40 +0000

    updated requirements.txt

--- pages/Profile.py
+++ pages/Profile.py
@@ -9,6 +9,9 @@ def app():
 
         new_weigh 
         )
+    option = ste.selectbox("How would you like to be contacted?", range(100),key="selectbox")
+
+    st.write("You selected:", option)
 
  
 app()
