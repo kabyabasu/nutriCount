02187f098833539b7407bae7f3955b535795c00f
KABYA BASU <kabyabasu@yahoo.co.uk>
2024-03-09 12:16:51 +0000

    updated requirements.txt

--- Profile.py
+++ Profile.py
@@ -1,5 +1,6 @@
 import streamlit as st
 from st_pages import add_page_title
+import streamlit-extras as ste
 
 add_page_title()
 
@@ -7,5 +8,11 @@ def app():
 
     Test = st.selectbox(
         "Are you Ready to Incorporate Exercise in your Daily routine", ["Yes", "No"]
-    )
+    ),
+    option = ste.selectbox("How would you like to be contacted?", range(100),key="selectbox")
+
+    st.write("You selected:", option)
+
+
+app()
  
