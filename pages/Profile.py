a2adfe80cde3799699a4fc187d40bfb3376b26d0
KABYA BASU <kabyabasu@yahoo.co.uk>
2024-03-10 16:26:30 +0000

    adding foods

--- pages/Profile.py
+++ pages/Profile.py
@@ -8,7 +8,7 @@ add_page_title()
 #     st.session_state[k] = v
 def app():
     #col1,col2 = st.columns([1,1])
-    st.markdown(#Summary Health Status)
+    st.markdown("#Summary Health Status")
     #b= bm(st.session_state["height"],st.session_state["weight"])
     bmi = bm(st.session_state["height"],st.session_state["weight"])
 
