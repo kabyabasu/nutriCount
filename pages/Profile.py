c4ebb679631aaab7373f57e934423e8a5c83cad3
KABYA BASU <kabyabasu@yahoo.co.uk>
2024-03-10 11:28:03 +0000

    updating basic assement

--- pages/Profile.py
+++ pages/Profile.py
@@ -10,27 +10,27 @@ def app():
     try:
 
         st.write(
-            "Hey",st.session_state['name'],"  \n"
+            "Hey",st.session_state['name'],"  \n",
 
             "Your weight is", st.session_state['weight'],".  \n",
-            "Your Height is" ,st.session_state['height'],'\n',
-            "Your current Occupation is", st.session_state['occupation'],
-            "Your current number of work hour is", st.session_state['duration_of_workday'],
-            "Your Gender is ",st.session_state['gender'],
-            "You are", st.session_state["pregnant"], "pregnent",
-            "You are", st.session_state["breastfeeding"], "breastfeeding",
-            "You have pain",st.session_state["pain"],
-            "Your pain intesity is", st.session_state["pain_intensity"],
-            "Your Pain Location is ", st.session_state["pain_location"][0],
-            "Your have tingling sension", st.session_state["tingling"],
-            "Your tignling intensity is ",st.session_state["tingling_intensity"],
-            "Your current tingling location is ", st.session_state["tingling_location"][0],
-            "You have weekness in", st.session_state["weekness"],
-            "Your weekness intensity is ",st.session_state["weekness_intensity"],
-            "Your current weekness location is ", st.session_state["weekness_location"][0],
-            "You have tightness in", st.session_state["tightness"],
-            "Your tightness intensity is ",st.session_state["tightness_intensity"],
-            "Your current tightness location is ", st.session_state["tightness_location"][0]
+            "Your Height is" ,st.session_state['height'],'  \n',
+            "Your current Occupation is", st.session_state['occupation'],".  \n",
+            "Your current number of work hour is", st.session_state['duration_of_workday'],".  \n",
+            "Your Gender is ",st.session_state['gender'],".  \n",
+            "You are", st.session_state["pregnant"], "pregnent",".  \n",
+            "You are", st.session_state["breastfeeding"], "breastfeeding",".  \n",
+            "You have pain",st.session_state["pain"],".  \n",
+            "Your pain intesity is", st.session_state["pain_intensity"],".  \n",
+            "Your Pain Location is ", st.session_state["pain_location"][0],".  \n",
+            "Your have tingling sension", st.session_state["tingling"],".  \n",
+            "Your tignling intensity is ",st.session_state["tingling_intensity"],".  \n",
+            "Your current tingling location is ", st.session_state["tingling_location"][0],".  \n",
+            "You have weekness in", st.session_state["weekness"],".  \n",
+            "Your weekness intensity is ",st.session_state["weekness_intensity"],".  \n",
+            "Your current weekness location is ", st.session_state["weekness_location"][0],".  \n",
+            "You have tightness in", st.session_state["tightness"],".  \n",
+            "Your tightness intensity is ",st.session_state["tightness_intensity"],".  \n",
+            "Your current tightness location is ", st.session_state["tightness_location"][0]".  \n",
             
             )
 
