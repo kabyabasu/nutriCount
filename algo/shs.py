5ac21394ec9f8baf180b1ecf3adfd6b9621e7205
KABYA BASU <kabyabasu@yahoo.co.uk>
2024-03-10 15:06:31 +0000

    adding other pages

--- algo/shs.py
+++ algo/shs.py
@@ -1,4 +1,17 @@
 import streamlit as st
-from pages.basic import current_weight
 
 #new_weigh = st.session_state.weight * 100
+
+def bm(D7,D9):
+        # Assuming D7, D8, and D9 are variables in Python that have been previously defined
+    D7 = 10  # Example value
+    D9 = 20  # Example value
+
+
+    result = D7 / ((D9 / 100) ** 2)
+
+    print(result)
+    return result
+
+bm = bm(st.session_state["height"],st.session_state["weight"])
+
