9bcf16df860389b907eda6125341238953c0e530
KABYA BASU <kabyabasu@yahoo.co.uk>
2024-03-10 09:40:00 +0000

    updating basic

--- pages/basic.py
+++ pages/basic.py
@@ -122,7 +122,7 @@ def wt():
             current_occupation = st.session_state["occupation"]
 
         else:
-            current_occupation = st.selectbox("Pick your Occupdation", ["working","Student"])
+            current_occupation = st.selectbox("Pick your Occupdation", ["Homebody Housepersons","Student"])
             st.session_state["occupation"] = current_occupation
 
         # if "occupation" in st.session_state:
