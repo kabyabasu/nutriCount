7421aa4282c6fd02f6178527430c709494da5c40
KABYA BASU <kabyabasu@yahoo.co.uk>
2024-03-11 00:52:21 +0000

    updating profile creating North Star Part

--- pages/recomandation.py
+++ pages/recomandation.py
@@ -2,5 +2,5 @@ import streamlit as st
 
 st.write(
 
-    "Our Team is Working Hard to Serve you this Page!! So come back to this page after a quick Nap"
+    f"Hey !! {st.session_state['name']} Our crack team of digital wizards is currently in overdrive, flexing their tech muscles to serve up this page on a silver platter just for you! We suggest you embark on a brief journey to the land of Nod. By the time you return, rubbing the sleep from your eyes, this page will be ready to dazzle you. So, grab your coziest blanket, and take a quick power nap. We'll be here, weaving internet magic!"
 )
